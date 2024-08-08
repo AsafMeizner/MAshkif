16d818776568d5473ef8dddd0dafb9672b3b4bda
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2024-08-08 16:33:06 +0300

    added electron native desktop app

---
+++ electron/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mashkif.app',
+  appName: 'MAshkif',
+  webDir: 'build'
+};
+
+export default config;
